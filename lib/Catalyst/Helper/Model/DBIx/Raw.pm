7b8a720a44f16cc12e6271a035748650155bef2c
Adam Hopkins <SirChickenHair@srchulo-aire.local>
2014-05-17 18:17:43 -0700

    Added helper and now model works

---
+++ lib/Catalyst/Helper/Model/DBIx/Raw.pm
@@ -0,0 +1,76 @@
+package Catalyst::Helper::Model::DBIx::Raw;
+use strict;
+ 
+#ABSTRACT: Helper for Net::Stripe Model
+
+=head1 SYNOPSIS
+ 
+  script/myapp_create.pl model Raw DBIx::Raw
+ 
+=head1 DESCRIPTION
+ 
+Helper for DBIx::Raw model
+ 
+=method mk_compclass
+ 
+=cut
+
+sub mk_compclass {
+  my ($self, $helper) = @_;
+  my $file = $helper->{file};
+  $helper->render_file('compclass', $file);
+}
+ 
+=head1 SEE ALSO
+ 
+L<Catalyst::Manual>, L<Catalyst::Test>, L<Catalyst::Request>,
+L<Catalyst::Response>, L<Catalyst::Helper>, L<Catalyst::Model::DBIx::Raw>
+ 
+=cut
+ 
+1;
+ 
+__DATA__
+=begin pod_to_ignore
+ 
+__compclass__
+package [% class %];
+ 
+use strict;
+use warnings;
+ 
+use base qw/Catalyst::Model::DBIx::Raw/;
+ 
+# 3-token (Signature) authentication
+__PACKAGE__->config(
+	dsn => 'dsn',
+	user => 'user',
+	password => 'password',
+	conf => '/path/to/conf.pl',
+	dbix_class_model => 'DB',
+);
+ 
+=head1 NAME
+ 
+[% class %] - Catalyst DBIx::Raw Model for [% app %]
+ 
+=head1 SYNOPSIS
+ 
+See L<[% app %]>
+ 
+=head1 DESCRIPTION
+ 
+Catalyst DBIx::Raw Model for [% app %]
+ 
+=head1 AUTHOR
+ 
+[% author %]
+ 
+=head1 LICENSE
+ 
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+ 
+=cut
+ 
+1;
--- lib/Catalyst/Model/DBIx/Raw.pm
+++ lib/Catalyst/Model/DBIx/Raw.pm
@@ -2,4 +2,69 @@ package Catalyst::Model::DBIx::Raw;
 use strict;
 use warnings;
 
+use Carp ();
+use DBIx::Raw;
+use Moose;
+extends 'Catalyst::Model';
+with 'Catalyst::Component::InstancePerContext';
+no Moose;
+
+#ABSTRACT: A Catalyst Model for DBIx::Raw
+
+sub new {
+	my $self  = shift->next::method(@_);
+	my $class = ref($self);
+    
+	my ($c) = @_;	
+	$self->_create_raw($c);
+    return $self;
+}
+
+sub _create_raw { 
+	my ($self, $c) = @_;
+
+	my %options;
+	if($self->{dbix_class_model}) { 
+		my $model = $c->model($self->{dbix_class_model});
+		#catalyst will throw it's own error if model does not exist
+		$options{dbh} = $c->model($self->{dbix_class_model})->storage->dbh;
+	}
+	else { 
+		Carp::croak("Must provide either dbix_class_model, or (dsn, user, password), or conf") unless $self->{dbix_class_model}
+					or ($self->{dsn} and $self->{user} and $self->{password}) or $self->{conf};
+		%options =	(
+						dns => $self->{dsn},
+						user => $self->{user},
+						password => $self->{password},
+						conf => $self->{conf},
+					);
+	}
+
+    # Instantiate a new DBIx::Raw object...
+    $self->{raw} = DBIx::Raw->new(%options);
+}
+
+sub raw { shift->{raw} }
+
+sub build_per_context_instance {
+	my ($self, $c) = @_;
+ 
+	return $self unless ref $c;
+	$self->_create_raw($c);
+	return $self;
+}
+
+sub AUTOLOAD {
+    my $self = shift;
+    my %args = @_;
+ 
+    our $AUTOLOAD;
+ 
+    my $program = $AUTOLOAD;
+    $program =~ s/.*:://;
+ 
+    # pass straight through to our DBIx::Raw object
+    return $self->{raw}->$program(%args);
+}
+
 1;
